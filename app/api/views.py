4bf7b6df1a054c0df6005308881f43377c6c4094
rinat <hgh.sgf2015@yadex.ru>
2024-06-11 16:28:16 +1000

    making auth to api

--- app/api/views.py
+++ app/api/views.py
@@ -1,10 +1,21 @@
 from rest_framework import generics
 from app.models import Add
 from app.api.serializers import AddSerializer
+from rest_framework.authentication import  BasicAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+
 
 class AddListView(generics.ListAPIView):
     queryset = Add.objects.all()
     serializer_class = AddSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 class AddDetailView(generics.RetrieveAPIView):
     queryset = Add.objects.all()
     serializer_class = AddSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
