94a1bbee76a4131757aaf6189d0c9cf85152c14e
rinat <hgh.sgf2015@yadex.ru>
2024-06-11 17:46:38 +1000

    some changes

--- app/api/views.py
+++ app/api/views.py
@@ -7,12 +7,14 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
-
+#представление для списка
 class AddListView(generics.ListAPIView):
     queryset = Add.objects.all()
     serializer_class = AddSerializer
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+
+#представление для каждого элемента списка
 class AddDetailView(generics.RetrieveAPIView):
     queryset = Add.objects.all()
     serializer_class = AddSerializer
