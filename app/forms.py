eb46f33385d0044fc879996254036be24f551361
rinat <hgh.sgf2015@yadex.ru>
2024-06-11 16:47:17 +1000

    user registration

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
--- app/views.py
+++ app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .forms import UserRegistrationForm
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            user_form = UserRegistrationForm(request.POST)
+
+            # Создать новый объект пользователя,
+            # но пока не сохранять его
+            new_user = user_form.save(commit=False)
+            # Установить выбранный пароль
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Сохранить объект User
+            new_user.save()
+
+
+            return render(request,
+                          'register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'register.html',
+                  {'user_form': user_form})
